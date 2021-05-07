5f08de1b3896cb91b439436a3fbceb03e2fe49eb
QHR0605 <1348357847@qq.com>
2021-05-07 21:25:12 +0800

    smtp常用命令常量

---
+++ src/main/java/com/example/server/util/CommandConstant.java
@@ -0,0 +1,17 @@
+package com.example.server.util;
+
+/**
+ * @author 全鸿润
+ */
+public class CommandConstant {
+
+    public static final String HELO = "HELO";
+    public static final String AUTH_LOGIN = "AUTH LOGIN";
+    public static final String MAIL_FROM = "MAIL FROM";
+    public static final String RCPT_TO = "RCPT TO";
+    public static final String DATA = "DATA";
+    public static final String RSET = "REST";
+    public static final String QUIT = "QUIT";
+
+
+}
