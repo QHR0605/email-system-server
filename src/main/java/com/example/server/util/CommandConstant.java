71d2c1741f96bce88af0cb5588617803af100dac
zfbz <891671942@qq.com>
2021-05-07 22:46:23 +0800

    pop3命令

--- src/main/java/com/example/server/util/CommandConstant.java
+++ src/main/java/com/example/server/util/CommandConstant.java
@@ -4,7 +4,7 @@ package com.example.server.util;
  * @author 全鸿润
  */
 public class CommandConstant {
-
+    // SMTP
     public static final String HELO = "HELO";
     public static final String AUTH_LOGIN = "AUTH LOGIN";
     public static final String MAIL_FROM = "MAIL FROM";
@@ -12,6 +12,13 @@ public class CommandConstant {
     public static final String DATA = "DATA";
     public static final String RSET = "REST";
     public static final String QUIT = "QUIT";
-
-
+    // POP3
+    public static final String USER = "USER";
+    public static final String PASS = "PASS";
+    public static final String STAT = "STAT";
+    public static final String LIST = "LIST";
+    public static final String RETR = "RETE";
+    public static final String DELE = "DELE";
+    public static final String REST = "REST";
+    // public static final String QUIT = "QUIT";
 }
