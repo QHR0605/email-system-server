4b594cf2188f5a3a10ac4ef8fe52c63b4d7dc2a9
QHR0605 <1348357847@qq.com>
2021-05-09 21:52:27 +0800

    Json状态码

---
+++ src/main/java/com/example/server/util/json/JsonResultStateCode.java
@@ -0,0 +1,11 @@
+package com.example.server.util.json;
+
+/**
+ * @author 全鸿润
+ */
+public class JsonResultStateCode {
+
+    static final Integer SUCCESS = 200;
+    static final Integer NOT_FOUND = 404;
+    static final Integer SERVER_ERROR = 500;
+}
