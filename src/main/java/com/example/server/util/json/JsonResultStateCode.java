1d59bb7b38d5e7d364797581de1627818c01f99f
QHR0605 <1348357847@qq.com>
2021-05-10 18:50:19 +0800

    新增状态码及其状态信息

--- src/main/java/com/example/server/util/json/JsonResultStateCode.java
+++ src/main/java/com/example/server/util/json/JsonResultStateCode.java
@@ -5,7 +5,18 @@ package com.example.server.util.json;
  */
 public class JsonResultStateCode {
 
-    static final Integer SUCCESS = 200;
-    static final Integer NOT_FOUND = 404;
-    static final Integer SERVER_ERROR = 500;
+    static public Integer SUCCESS = 200;
+    static public String SUCCESS_DESC = "success";
+
+    public static Integer USERNAME_WRONG = 410;
+    public static String USERNAME_WRONG_DESC = "用户名错误";
+
+    public static Integer USERNAME_IS_EXITED = 413;
+    public static String USERNAME_IS_EXITED_DESC = "用户名已存在";
+
+    static public Integer UNKNOWN_ERROR = 404;
+    static public String UNKNOWN_ERROR_DESC = "unknown error";
+
+    public static Integer PASSWORD_WRONG = 414;
+    public static String PASSWORD_WRONG_DESC = "密码错误";
 }
--- src/main/java/com/example/server/util/json/SmtpStateCode.java
+++ src/main/java/com/example/server/util/json/SmtpStateCode.java
@@ -7,9 +7,14 @@ public class SmtpStateCode {
 
     public static final int READY = 220;
     public static final int SUCCESS = 250;
+    public static final String SUCCESS_DESC = "OK\r\n";
     public static final int AUTH_SUCCESS = 235;
+    public static final int AUTH_FAILED = 535;
     public static final int PROCESSING= 221;
     public static final int SENDING = 354;
     public static final int COMMAND_ERROR = 500;
+    public static final String COMMAND_ERROR_DESC = COMMAND_ERROR+" Syntax\r\n";
     public static final int INTERRUPTED = 552;
+    public static final int LOGICAL_ERROR = 503;
+
 }
