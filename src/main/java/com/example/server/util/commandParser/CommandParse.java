292fd9d2653f294bcf42e30ef605845b3e068c94
QHR0605 <1348357847@qq.com>
2021-05-10 18:52:12 +0800

    指令解析工具类

---
+++ src/main/java/com/example/server/util/commandParser/CommandParse.java
@@ -0,0 +1,18 @@
+package com.example.server.util.commandParser;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * @author 全鸿润
+ */
+public class CommandParse {
+
+    public static String[] parseCommand(String command){
+        String[] args;
+        if (StringUtils.isEmpty(command) || command.startsWith(" ")){
+            return null;
+        }
+        args = command.split("//s");
+        return args;
+    }
+}
