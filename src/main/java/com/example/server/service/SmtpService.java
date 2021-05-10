9eb12b50d32b5290b534a86b3376dc48a10611a7
QHR0605 <1348357847@qq.com>
2021-05-10 18:51:50 +0800

    修改指令的处理函数

--- src/main/java/com/example/server/service/SmtpService.java
+++ src/main/java/com/example/server/service/SmtpService.java
@@ -1,5 +1,7 @@
 package com.example.server.service;
 
+import com.example.server.entity.SmtpResult;
+
 /**
  * @author 全鸿润
  */
@@ -9,35 +11,35 @@ public interface SmtpService {
      * 处理Helo指令
      * @return SMTP指令的状态码
      */
-    Integer handleHelloCommand();
+    SmtpResult handleHelloCommand(String[] args);
     /**
      * 处理AUTH LOGIN指令
      * @return SMTP指令的状态码
      */
-    Integer handleAuthCommand();
+    SmtpResult handleAuthCommand(String[] args);
     /**
      * 处理MAIL FROM指令
      * @return SMTP指令的状态码
      */
-    Integer handleMailCommand();
+    SmtpResult handleMailCommand(String[] args);
     /**
      * 处理RCPT TO指令
      * @return SMTP指令的状态码
      */
-    Integer handleRcptCommand();
+    SmtpResult handleRcptCommand(String[] args);
     /**
      * 处理DATA指令
      * @return SMTP指令的状态码
      */
-    Integer handleDataCommand();
+    SmtpResult handleDataCommand(String[] args);
     /**
      * 处理Reset指令
      * @return SMTP指令的状态码
      */
-    Integer handleResetCommand();
+    SmtpResult handleResetCommand(String[] args);
     /**
      * 处理QUIT指令
      * @return SMTP指令的状态码
      */
-    Integer handleQuitCommand();
+    SmtpResult handleQuitCommand(String[] args);
 }
