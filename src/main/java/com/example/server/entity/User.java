bf9ee3f8e4d4235d6de7306095421156fb401b17
QHR0605 <1348357847@qq.com>
2021-05-10 18:54:35 +0800

    用户实体

---
+++ src/main/java/com/example/server/entity/User.java
@@ -0,0 +1,82 @@
+package com.example.server.entity;
+
+import java.sql.Timestamp;
+
+/**
+ * @author 全鸿润
+ */
+public class User {
+
+    private String username;
+    private String password;
+    private String phone;
+    private Boolean accountType;
+    private Timestamp latestLoginTime;
+    private String latestLoginIp;
+    private Integer mailBoxSize;
+    private String avatarUrl;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(Boolean accountType) {
+        this.accountType = accountType;
+    }
+
+    public Timestamp getLatestLoginTime() {
+        return latestLoginTime;
+    }
+
+    public void setLatestLoginTime(Timestamp latestLoginTime) {
+        this.latestLoginTime = latestLoginTime;
+    }
+
+    public String getLatestLoginIp() {
+        return latestLoginIp;
+    }
+
+    public void setLatestLoginIp(String latestLoginIp) {
+        this.latestLoginIp = latestLoginIp;
+    }
+
+    public Integer getMailBoxSize() {
+        return mailBoxSize;
+    }
+
+    public void setMailBoxSize(Integer mailBoxSize) {
+        this.mailBoxSize = mailBoxSize;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
