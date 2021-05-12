0a9424af8153841a909550620991f7487cbe183c
QHR0605 <1348357847@qq.com>
2021-05-12 15:18:17 +0800

    自定义Spring自动注入

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -1,15 +1,22 @@
 package com.example.server;
 
+import com.example.server.config.SpringContextConfig;
+import com.example.server.server.SmtpServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.server.mapper")
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ServerApplication.class, args);
+        SpringContextConfig contextConfig = new SpringContextConfig();
+        contextConfig.setApplicationContext(context);
+        SmtpServer smtpServer = new SmtpServer();
+        smtpServer.start();
+    }
 
 }
