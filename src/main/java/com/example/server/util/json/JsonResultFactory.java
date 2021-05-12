8f952e466a28d9d6553d66cea1147a592011ffab
QHR0605 <1348357847@qq.com>
2021-05-12 15:17:14 +0800

    修改了工厂方法

--- src/main/java/com/example/server/util/json/JsonResultFactory.java
+++ src/main/java/com/example/server/util/json/JsonResultFactory.java
@@ -7,16 +7,19 @@ import com.example.server.entity.SmtpResult;
  */
 public class JsonResultFactory {
 
-    public static JsonResult buildJsonResult(Integer stateCode , String message , Object body){
-        return new JsonResult(stateCode,message,body);
+    public static JsonResult buildJsonResult(Integer stateCode, String message, Object body) {
+        return new JsonResult(stateCode, message, body);
     }
-    public static JsonResult buildSuccessResult(SmtpResult smtpResult){
-        return buildJsonResult(JsonResultStateCode.SUCCESS,"",smtpResult);
+
+    public static JsonResult buildSuccessResult(SmtpResult smtpResult) {
+        return buildJsonResult(JsonResultStateCode.SUCCESS, "", smtpResult);
     }
-    public static JsonResult buildSuccessResult(){
-        return buildJsonResult(JsonResultStateCode.SUCCESS,"",null);
+
+    public static JsonResult buildSuccessResult() {
+        return buildJsonResult(JsonResultStateCode.SUCCESS, "", null);
     }
-    public static JsonResult buildSmtpCommandErrorResult(){
-        return buildSuccessResult(new SmtpResult(SmtpStateCode.COMMAND_ERROR,SmtpStateCode.COMMAND_ERROR_DESC));
+
+    public static JsonResult buildSmtpCommandErrorResult() {
+        return buildSuccessResult(new SmtpResult(SmtpStateCode.COMMAND_ERROR, SmtpStateCode.COMMAND_ERROR_DESC));
     }
 }
