7069bf9d7e46d69f27eadec7747f0e3c4dd79b9b
QHR0605 <1348357847@qq.com>
2021-05-12 15:19:55 +0800

    新增SMRP状态码

--- src/main/java/com/example/server/util/json/SmtpStateCode.java
+++ src/main/java/com/example/server/util/json/SmtpStateCode.java
@@ -6,15 +6,32 @@ package com.example.server.util.json;
 public class SmtpStateCode {
 
     public static final int READY = 220;
+
     public static final int SUCCESS = 250;
-    public static final String SUCCESS_DESC = "OK\r\n";
+    public static final String SUCCESS_DESC = " OK";
+
     public static final int AUTH_SUCCESS = 235;
+    public static final String AUTH_SUCCESS_DESC = AUTH_SUCCESS + " Authentication successful";
+
     public static final int AUTH_FAILED = 535;
-    public static final int PROCESSING= 221;
+    public static final String AUTH_FAILED_DESC = AUTH_FAILED + " Error: Authentication failed";
+
+    public static final int PROCESSING = 221;
     public static final int SENDING = 354;
+
     public static final int COMMAND_ERROR = 500;
-    public static final String COMMAND_ERROR_DESC = COMMAND_ERROR+" Syntax\r\n";
+    public static final String COMMAND_ERROR_DESC = COMMAND_ERROR + " Error: bad Syntax";
+
     public static final int INTERRUPTED = 552;
-    public static final int LOGICAL_ERROR = 503;
+    public static final int SEQUENCE_ERROR = 503;
+    public static final String SEQUENCE_ERROR_DESC = SEQUENCE_ERROR + " bad sequence of commands";
+
+    public static final int AUTH_PROCESS = 334;
+    public static final String USERNAME_SENT_DESC = AUTH_PROCESS + " dXNlcm5hbWU6";
+    public static final String PASSWORD_SENT_DESC = AUTH_PROCESS + " cGFzc3dvcmQ6";
+
+    public static final int ADDRESS_NOT_AVAILABLE = 450;
+    public static final String ADDRESS_NOT_AVAILABLE_DESC = ADDRESS_NOT_AVAILABLE + " RCPT address is not available";
+
 
 }
