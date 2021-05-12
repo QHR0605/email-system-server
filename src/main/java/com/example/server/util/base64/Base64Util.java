55d4e17738cb641d5d31cb2880ca2944b5272754
QHR0605 <1348357847@qq.com>
2021-05-12 15:16:19 +0800

    修改了编码和解码方式

--- src/main/java/com/example/server/util/base64/Base64Util.java
+++ src/main/java/com/example/server/util/base64/Base64Util.java
@@ -1,6 +1,8 @@
 package com.example.server.util.base64;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 
 /**
@@ -9,23 +11,26 @@ import java.util.Base64.Encoder;
 public class Base64Util {
 
     private static final Encoder ENCODER = Base64.getEncoder();
+    private static final Decoder DECODER = Base64.getDecoder();
 
     /**
      * 编码
+     *
      * @param data 加密的内容
      * @return 加密后的字符串
      */
-    public static String encodeByBase64(byte[] data){
+    public static String encodeByBase64(byte[] data) {
 
         return ENCODER.encodeToString(data);
     }
 
     /**
      * 解码
+     *
      * @param data 解码的内容
      * @return 解码后的内容
      */
-    public static String decodeByBase64(byte[] data){
-        return ENCODER.encodeToString(data);
+    public static String decodeByBase64(byte[] data) {
+        return new String(DECODER.decode(data), StandardCharsets.UTF_8);
     }
 }
