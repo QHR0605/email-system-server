52c01d09c7ec24d4c4505689b72df340dc7b4069
QHR0605 <1348357847@qq.com>
2021-05-12 15:16:10 +0800

    修改了编码和解码方式

---
+++ src/main/java/com/example/server/service/AuthService.java
@@ -0,0 +1,26 @@
+package com.example.server.service;
+
+import com.example.server.entity.User;
+
+/**
+ * @author 全鸿润
+ */
+public interface AuthService {
+    /**
+     * 登录验证
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @return 验证状态
+     */
+    String handleLogin(String username, String password);
+
+    /**
+     * 查询用户
+     *
+     * @param username 用户名
+     * @return 用户实例
+     */
+    User findUserByUsername(String username);
+
+}
--- src/main/java/com/example/server/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/server/service/impl/AuthServiceImpl.java
@@ -1,41 +1,46 @@
 package com.example.server.service.impl;
 
-/**
- * @author 全鸿润
- */
-
+import com.example.server.config.SpringContextConfig;
 import com.example.server.entity.User;
 import com.example.server.mapper.LoginMapper;
-import com.example.server.service.LoginService;
+import com.example.server.service.AuthService;
 import com.example.server.util.base64.Base64Util;
 import com.example.server.util.json.JsonResultStateCode;
 import com.example.server.util.json.SmtpStateCode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Base64;
-
+/**
+ * @author 全鸿润
+ */
 @Service
-public class AuthServiceImpl implements LoginService {
-
-    @Autowired
-    private LoginMapper loginMapper;
+public class AuthServiceImpl implements AuthService {
+    private final LoginMapper loginMapper = SpringContextConfig.getBean(LoginMapper.class);
 
     @Override
-    public Integer handleLogin(String encodedUsername, String encodedPassword) {
+    public String handleLogin(String encodedUsername, String encodedPassword) {
 
         User user;
         String username = Base64Util.decodeByBase64(encodedUsername.getBytes());
         String password = Base64Util.decodeByBase64(encodedUsername.getBytes());
         try {
             user = loginMapper.findUserByUserName(username);
-            if (user != null && user.getPassword().equals(password)){
-                return SmtpStateCode.AUTH_SUCCESS;
+            if (user != null && user.getPassword().equals(password)) {
+                return SmtpStateCode.AUTH_SUCCESS_DESC;
             }
-            return SmtpStateCode.AUTH_FAILED;
+            return SmtpStateCode.AUTH_FAILED_DESC;
         } catch (Exception e) {
             e.printStackTrace();
-            return JsonResultStateCode.UNKNOWN_ERROR;
+            return JsonResultStateCode.UNKNOWN_ERROR_DESC;
+        }
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        try {
+            return loginMapper.findUserByUserName(username);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
