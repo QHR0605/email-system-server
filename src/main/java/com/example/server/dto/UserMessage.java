b339edc4d2581d400dcc6715a314159a0e0e6435
QHR0605 <1348357847@qq.com>
2021-05-13 21:14:06 +0800

    前端发送的用户信息封装

---
+++ src/main/java/com/example/server/dto/UserMessage.java
@@ -0,0 +1,73 @@
+package com.example.server.dto;
+
+import java.sql.Timestamp;
+
+/**
+ * @author 全鸿润
+ */
+public class UserMessage {
+
+    private String username;
+    private String password;
+    private String phone;
+    private Boolean accountType;
+    private Timestamp latestLoginTime;
+    private String latestLoginIp;
+    private String avatarUrl;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(Boolean accountType) {
+        this.accountType = accountType;
+    }
+
+    public Timestamp getLatestLoginTime() {
+        return latestLoginTime;
+    }
+
+    public void setLatestLoginTime(Timestamp latestLoginTime) {
+        this.latestLoginTime = latestLoginTime;
+    }
+
+    public String getLatestLoginIp() {
+        return latestLoginIp;
+    }
+
+    public void setLatestLoginIp(String latestLoginIp) {
+        this.latestLoginIp = latestLoginIp;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
