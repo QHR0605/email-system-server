764a645bd065f1d07a417d20d66304a0e73dc7a5
QHR0605 <1348357847@qq.com>
2021-05-13 19:42:20 +0800

    修改了客户端测试程序

--- src/main/java/com/example/server/client/SmtpClient.java
+++ src/main/java/com/example/server/client/SmtpClient.java
@@ -1,6 +1,6 @@
 package com.example.server.client;
 
-import org.springframework.stereotype.Component;
+import com.example.server.util.json.SmtpStateCode;
 
 import java.io.*;
 import java.net.Socket;
@@ -23,12 +23,11 @@ public class SmtpClient extends Thread {
             Socket socket = new Socket("127.0.0.1", 25);
             handle(socket);
         } catch (IOException e) {
-            System.out.println("SMTP 服务器未开启，或请检查连接的端口号");
             e.printStackTrace();
         }
     }
 
-    public void handle(Socket socket){
+    public void handle(Socket socket) {
 
         Thread t1 = new Thread(new Runnable() {
             @Override
@@ -40,33 +39,33 @@ public class SmtpClient extends Thread {
                     PrintWriter writer = new PrintWriter(new OutputStreamWriter(out));
                     Scanner scanner = new Scanner(System.in);
                     System.out.println(reader.readLine());
-                    while (true){
-                        // 接收用户 cmd 输入
+                    while (true) {
                         String output = scanner.nextLine();
-                        // 客户端发送的、用于启动邮件内容传输的命令
-                        if ("DATA".equals(output)){
+                        if ("DATA".equals(output)) {
                             writer.println(output);
                             writer.flush();
-                            System.out.println(reader.readLine());
-                            // 拼接邮件
-                            while (true){
-                                String content = scanner.nextLine();
-                                if (".".equals(content)){
-                                    writer.append(content).append("\n"); // 感觉这个不用拼接
-                                    writer.flush();
-                                    break;
+                            String line = reader.readLine();
+                            System.out.println(line);
+                            if (line != null && line.startsWith(Integer.toString(SmtpStateCode.START_EMAIL_INPUT))) {
+                                while (true) {
+                                    String content = scanner.nextLine();
+                                    if (".".equals(content)) {
+                                        writer.append(content + "\n");
+                                        writer.flush();
+                                        break;
+                                    }
+                                    writer.append(content + "\n");
                                 }
-                                writer.append(content).append("\n");
+                                System.out.println(reader.readLine());
                             }
-                            System.out.println(reader.readLine());
                             continue;
                         }
                         writer.println(output);
                         writer.flush();
-                        String s = reader.readLine(); // 接收服务器响应
+                        String s = reader.readLine();
                         System.out.println(s);
                     }
-                }catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
