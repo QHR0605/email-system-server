9834c73c5d48832c0236f9b60fd27c756ff28bd0
zfbz <891671942@qq.com>
2021-05-13 12:09:05 +0800

    add pop3

---
+++ src/main/java/com/example/server/service/Pop3Service.java
@@ -0,0 +1,33 @@
+package com.example.server.service;
+
+import com.example.server.dto.Pop3Session;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public abstract class Pop3Service {
+    protected Socket socket;
+    protected PrintWriter writer;
+    protected BufferedReader reader;
+    protected Pop3Session pop3Session;
+
+    public Pop3Service(Socket socket, Pop3Session pop3Session) {
+        this.socket = socket;
+        this.pop3Session = pop3Session;
+    }
+
+    abstract public void handleUserCommand(String[] args);
+
+    abstract public void handlePassCommand(String[] args);
+
+    abstract public void handleStatCommand(String[] args);
+
+    abstract public void handleListCommand(String[] args);
+
+    abstract public void handleRetrCommand(String[] args);
+
+    abstract public void handleDeleCommand(String[] args);
+
+    abstract public void handleRestCommand(String[] args);
+}
---
+++ src/main/java/com/example/server/service/impl/Pop3ServiceImpl.java
@@ -0,0 +1,61 @@
+package com.example.server.service.impl;
+
+import com.example.server.config.SpringContextConfig;
+import com.example.server.dto.Pop3Session;
+import com.example.server.mapper.MailMapper;
+import com.example.server.service.AuthService;
+import com.example.server.service.Pop3Service;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Pop3ServiceImpl extends Pop3Service {
+
+    private final AuthService authService = SpringContextConfig.getBean("AuthServiceImpl");
+    private final MailMapper mailMapper = SpringContextConfig.getBean(MailMapper.class);
+
+    public Pop3ServiceImpl(Socket socket, Pop3Session pop3Session) {
+        super(socket, pop3Session);
+        try {
+            this.writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleUserCommand(String[] args) {
+
+    }
+
+    @Override
+    public void handlePassCommand(String[] args) {
+
+    }
+
+    @Override
+    public void handleStatCommand(String[] args) {
+
+    }
+
+    @Override
+    public void handleListCommand(String[] args) {
+
+    }
+
+    @Override
+    public void handleRetrCommand(String[] args) {
+
+    }
+
+    @Override
+    public void handleDeleCommand(String[] args) {
+
+    }
+
+    @Override
+    public void handleRestCommand(String[] args) {
+
+    }
+}
