b0e5a50fe9ad6e737323ca71d2515a46e3fe9cbb
QHR0605 <1348357847@qq.com>
2021-05-15 16:28:24 +0800

    切面的处理逻辑

---
+++ src/main/java/com/example/server/util/aspect/LoginAspect.java
@@ -0,0 +1,51 @@
+package com.example.server.util.aspect;
+
+import com.example.server.util.annotation.IsLogin;
+import com.example.server.util.http.CookieUtil;
+import com.example.server.util.http.HttpUtil;
+import com.example.server.util.json.JsonResultFactory;
+import com.example.server.util.json.JsonResultStateCode;
+import com.example.server.util.token.TokenVerifier;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+
+/**
+ * @author 全鸿润
+ */
+@Component
+@Aspect
+public class LoginAspect {
+
+    @Pointcut(value = "@annotation(isLogin)", argNames = "isLogin")
+    public void print(IsLogin isLogin) {
+    }
+
+    @Around(value = "print(isLogin)", argNames = "point,isLogin")
+    public Object authToken(ProceedingJoinPoint point, IsLogin isLogin) {
+        try {
+            Cookie[] cookies = HttpUtil.getRequest().getCookies();
+            Cookie cookie = CookieUtil.getCookie(cookies, "token");
+            if (cookie == null) {
+                return JsonResultFactory.buildJsonResult(JsonResultStateCode.UNAUTHORIZED, JsonResultStateCode.UNAUTHORIZED_DESC, null);
+            }
+            String token = cookie.getValue();
+            if (token == null) {
+                return JsonResultFactory.buildJsonResult(JsonResultStateCode.UNAUTHORIZED, JsonResultStateCode.UNAUTHORIZED_DESC, null);
+            } else {
+                if (TokenVerifier.verifyToken(token)) {
+                    return point.proceed();
+                } else {
+                    return JsonResultFactory.buildJsonResult(JsonResultStateCode.UNAUTHORIZED, JsonResultStateCode.UNAUTHORIZED_DESC, null);
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return JsonResultFactory.buildJsonResult(JsonResultStateCode.UNAUTHORIZED, JsonResultStateCode.UNAUTHORIZED_DESC, null);
+        }
+    }
+}
