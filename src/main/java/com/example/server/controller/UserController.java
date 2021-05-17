5e372dddbf933b9de89389a8ac6d2b7fae51ea77
QHR0605 <1348357847@qq.com>
2021-05-17 20:03:02 +0800

    登录用户权限操作

--- src/main/java/com/example/server/controller/UserController.java
+++ src/main/java/com/example/server/controller/UserController.java
@@ -5,14 +5,17 @@ import com.example.server.entity.User;
 import com.example.server.service.UserService;
 import com.example.server.service.impl.UserServiceImpl;
 import com.example.server.util.annotation.IsLogin;
-import com.example.server.util.http.CookieUtil;
+import com.example.server.util.http.CookieUtils;
 import com.example.server.util.json.JsonResult;
 import com.example.server.util.json.JsonResultFactory;
 import com.example.server.util.json.JsonResultStateCode;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -26,13 +29,19 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService = SpringContextConfig.getBean(UserServiceImpl.class);
+
     @GetMapping("/get-contact")
     @IsLogin
-    public JsonResult handleGetContact(HttpServletRequest request){
+    public JsonResult handleGetContact(HttpServletRequest request) {
 
         String username = null;
         try {
-            username = CookieUtil.getCookie(request.getCookies(),"username").getValue();
+            Cookie cookie = CookieUtils.findCookie(request.getCookies(), "username");
+            if (cookie == null) {
+                return JsonResultFactory.buildJsonResult(JsonResultStateCode.UNAUTHORIZED, JsonResultStateCode.UNAUTHORIZED_DESC, null);
+            } else {
+                username = cookie.getValue();
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return JsonResultFactory.buildJsonResult(
@@ -42,14 +51,14 @@ public class UserController {
             );
         }
         List<User> contactList = userService.getContactList(username);
-        if (contactList != null){
-            if (contactList.size()>0){
+        if (contactList != null) {
+            if (contactList.size() > 0) {
                 return JsonResultFactory.buildJsonResult(
                         JsonResultStateCode.SUCCESS,
                         JsonResultStateCode.SUCCESS_DESC,
                         contactList
                 );
-            }else{
+            } else {
                 return JsonResultFactory.buildJsonResult(
                         JsonResultStateCode.NOT_FOUND,
                         JsonResultStateCode.NOT_FOUND_DESC,
