664ded7fa6225ba0b960c42fc18710c1f99b61ed
QHR0605 <1348357847@qq.com>
2021-05-18 18:45:37 +0800

    IP地址拦截器

---
+++ src/main/java/com/example/server/util/interceptor/WebInterceptor.java
@@ -0,0 +1,67 @@
+package com.example.server.util.interceptor;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.server.config.SpringContextConfig;
+import com.example.server.entity.Filter;
+import com.example.server.mapper.SuperAdminMapper;
+import com.example.server.util.json.JsonResult;
+import com.example.server.util.json.JsonResultFactory;
+import com.example.server.util.json.JsonResultStateCode;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * @author 全鸿润
+ */
+public class WebInterceptor implements HandlerInterceptor {
+
+    private final SuperAdminMapper superAdminMapper = SpringContextConfig.getBean(SuperAdminMapper.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String ip = IpUtil.getIp(request);
+        JsonResult jsonResult = JsonResultFactory.buildJsonResult(
+                JsonResultStateCode.INTERRUPTED,
+                JsonResultStateCode.INTERRUPTED_DES,
+                null
+        );
+        JSONObject jsonObject = (JSONObject) JSONObject.toJSON(jsonResult);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        PrintWriter writer = null;
+        System.out.println("IP地址:"+ip);
+        if (ip == null || ip.isEmpty()){
+            writer = response.getWriter();
+            writer.append(jsonObject.toJSONString());
+            writer.flush();
+            return false;
+        }
+        List<Filter> filters = superAdminMapper.selectFilter();
+        for (Filter  filter: filters
+             ) {
+            if (filter.getIpAddress().equals(ip)){
+                writer = response.getWriter();
+                writer.append(jsonObject.toJSONString());
+                writer.flush();
+                System.out.println("拦截:"+ip);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
