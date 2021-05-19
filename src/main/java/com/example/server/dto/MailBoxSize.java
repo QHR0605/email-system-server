1820ec8cce6a2afa122bad7955c60a6780f25f78
QHR0605 <1348357847@qq.com>
2021-05-20 01:05:05 +0800

    邮箱信息类

---
+++ src/main/java/com/example/server/dto/MailBoxSize.java
@@ -0,0 +1,25 @@
+package com.example.server.dto;
+
+/**
+ * @author 全鸿润
+ */
+public class MailBoxSize {
+    private String username;
+    private Integer size;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+}
